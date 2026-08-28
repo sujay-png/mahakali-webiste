0ca847289996ea4125fb987699cfcddc594d5577
sujay.png <147098677+sujay-png@users.noreply.github.com>
2026-08-28 12:57:14 +0530

    Update Astro transitions API

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   site: "https://mahakalihomefurnitures.com",
-  output: "hybrid",
+
+  output: "server",
+
   adapter: vercel(),
+
   integrations: [react()],
+
   compressHTML: true,
+
   build: {
     inlineStylesheets: "auto",
   },
